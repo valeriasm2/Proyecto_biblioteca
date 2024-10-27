6cc4ec0190a7a8895bb228472cc21c0b25551235
graciacr <graciacr2004@gmail.com>
2024-10-27 13:26:49 +0100

    Moverse entre menús
    He añadido la parte de poder moverse entre menús, comprobación de opciones correctas y he añadido comentarios para que no haya pérdida.

--- biblio.py
+++ biblio.py
@@ -26,47 +26,323 @@ dic_users = {'1233423S':{'nombre':'Juan Perez', 'edad':24, 'tfn':456454847, 'mai
              }
 
 
-#MENÚ PRINCIPAL
-flg00 = True
-menu00 = " Menú princilal ".center(40,"=") + "\n" + "1) Gestión de Libros" + "\n" + \
-         "2) Gestión de Usuarios" + "\n" "3) Gestión de Géneros" + "\n"
+# MENÚ PRINCIPAL
+menu00 = " Menú principal ".center(40, "=") + "\n" + "1) Gestión de Libros" + "\n" + \
+         "2) Gestión de Usuarios" + "\n" + "3) Gestión de Géneros" + "\n" + "4) Exit" + "\n"
 
-#MENÚS DICCIONARIO LIBROS
-flg01 = False
-menu01 = " Gestión de Libros ".center(40,"=") + "\n" + "1) Listar todos los libros" + "\n" + \
-         "2) Buscar libro" + "\n" + "3) Editar libro"
-flg012 = False
-menu012 = " Buscar sibro ".center(40,"=") + "\n" + "1) Por título" + "\n" + "2)Por autor" + \
-          "\n" + "3)Por género" + "\n"
-flg013 = False
-menu013 = " Editar libro ".center(40,"=") + "\n" + "1) Añadir nuevo libro" + "\n" + "2) Editar libro existente" + \
-          "\n" + "3) Eliminar libro" + "\n"
-flg0132 = False
-menu0132 = " Editar libro existente ".center(40,"=") + "\n" + "1) Editar título" + "\n" + "2) Editar género"
+# MENÚS DICCIONARIO LIBROS
+menu01 = " Gestión de Libros ".center(40, "=") + "\n" + "1) Listar todos los libros" + "\n" + \
+         "2) Buscar libro" + "\n" + "3) Editar libro" + "\n" + "4) Volver atrás"
+menu012 = " Buscar libro ".center(40, "=") + "\n" + "1) Por título" + "\n" + "2) Por autor" + \
+          "\n" + "3) Por género" + "\n" + "4) Volver atrás"
+menu013 = " Editar libro ".center(40, "=") + "\n" + "1) Añadir nuevo libro" + "\n" + "2) Editar libro existente" + \
+          "\n" + "3) Eliminar libro" + "\n" + "4) Volver atrás"
+menu0132 = " Editar libro existente ".center(40, "=") + "\n" + "1) Editar título" + "\n" + "2) Editar género" + \
+           "\n" + "3) Volver atrás"
 
-#MENÚS GÉNEROS
-flg02 = False
-menu02 = " Gestión de usuarios ".center(40,"=") + "\n" + "1) Listar usuarios" + "\n" + "2) Buscar usuarios" + \
-         "\n" + "3) Editar usuarios"
-flg021 = False
-menu021 = " Listar usuarios ".center(40,"=") + "\n" + "1) Por DNI" + "\n" + "2) Por nombre" + "\n" + \
-          "3) Por cantidad de libros prestados" + "\n" + "4) Por cantidad de libros leidos" + "\n"
-flg022 = False
-menu022 = " Buscar usuarios ".center(40,"=") + "\n" + "1) Por nombre" + "\n" + "2) Por edad" + "\n"
-flg023 = False
-menu023 = " Editar usuarios ".center(40,"=") + "\n" + "1) Añadir usuario nuevo" + "\n" + "2) Modificar usuario existente" + \
-          "\n" + "3) Eliminar usuario" + "\n"
-flg0232 = False
-menu0232 = " Modificar usuario existente ".center(40,"=") + "\n" + "1) Editar datos personales" + "\n" + "2) Editar préstamos" + \
-           "\n" + "3) Añadir libro leídos" + "\n"
-flg02321 = False
-menu02321 = " Editar datos personales " + "\n" + "1) Teléfono" + "\n" + "2) Email" + "\n"
-flg02322 = False
-menu02322 = " Editar préstamos " + "\n" + "1) Añadir libro prestado" + "\n" + "2) Eliminar libro prestado" + "\n"
+# MENÚS USUARIOS
+menu02 = " Gestión de usuarios ".center(40, "=") + "\n" + "1) Listar usuarios" + "\n" + "2) Buscar usuarios" + \
+         "\n" + "3) Editar usuarios" + "\n" + "4) Volver atrás"
+menu021 = " Listar usuarios ".center(40, "=") + "\n" + "1) Por DNI" + "\n" + "2) Por nombre" + "\n" + \
+          "3) Por cantidad de libros prestados" + "\n" + "4) Por cantidad de libros leídos" + "\n" + "5) Volver atrás"
+menu022 = " Buscar usuarios ".center(40, "=") + "\n" + "1) Por nombre" + "\n" + "2) Por edad" + "\n" + "3) Volver atrás"
+menu023 = " Editar usuarios ".center(40, "=") + "\n" + "1) Añadir usuario nuevo" + "\n" + "2) Modificar usuario existente" + \
+          "\n" + "3) Eliminar usuario" + "\n" + "4) Volver atrás"
+menu0232 = " Modificar usuario existente ".center(40, "=") + "\n" + "1) Editar datos personales" + "\n" + "2) Editar préstamos" + \
+           "\n" + "3) Añadir libro leído" + "\n" + "4) Volver atrás"
+menu02321 = " Editar datos personales ".center(40, "=") + "\n" + "1) Teléfono" + "\n" + "2) Email" + "\n" + "3) Volver atrás"
+menu02322 = " Editar préstamos ".center(40, "=") + "\n" + "1) Añadir libro prestado" + "\n" + "2) Eliminar libro prestado" + \
+            "\n" + "3) Volver atrás"
 
-#MENÚS USERS
-flg03 = False
-menu03 = " Gestión de géneros ".center(40,"=") + "\n" + "1) Listar todos los géneros" + "\n" + "2) Añadir nuevo género" + \
-         "\n" + "3) Editar género" + "\n" + "4) Eliminar género" + "\n"
+# MENÚS GÉNEROS
+menu03 = " Gestión de géneros ".center(40, "=") + "\n" + "1) Listar todos los géneros" + "\n" + "2) Añadir nuevo género" + \
+         "\n" + "3) Editar género" + "\n" + "4) Eliminar género" + "\n" + "5) Volver atrás"
 
+# Inicialización de flgs
+flg00 = True  # Menú principal
+flg01 = False  # Gestión de Libros
+flg02 = False  # Gestión de Usuarios
+flg03 = False  # Gestión de Géneros
+flg012 = False  # Buscar libro
+flg013 = False  # Editar libro
+flg0132 = False  # Editar libro existente
+flg021 = False  # Listar usuarios
+flg022 = False  # Buscar usuarios
+flg023 = False  # Editar usuarios
+flg0232 = False  # Modificar usuario existente
+flg02321 = False  # Editar datos personales
+flg02322 = False  # Editar datos prestamos
 
+# Menú principal
+while flg00:
+    print(menu00)
+    opc = input("Opción: ")
+    if not opc.isdigit():
+        print("La opción debe ser numérica.")
+    elif int(opc) < 1 or int(opc) > 4:
+        print("Opción fuera de rango.")
+    else:
+        opc = int(opc)
+        if opc == 1:
+            flg00 = False
+            flg01 = True
+        elif opc == 2:
+            flg00 = False
+            flg02 = True
+        elif opc == 3:
+            flg00 = False
+            flg03 = True
+        elif opc == 4:
+            print("Saliendo del programa...")
+            flg00 = False
+
+    # Sección de gestión de libros
+    while flg01:
+        print(menu01)
+        opc = input("Opción: ")
+        if not opc.isdigit():
+            print("La opción debe ser numérica.")
+        elif int(opc) < 1 or int(opc) > 4:
+            print("Opción fuera de rango.")
+        else:
+            opc = int(opc)
+            if opc == 1:
+                print("Listar todos los libros")
+            elif opc == 2:
+                print("Buscar libro")
+                flg01 = False
+                flg012 = True
+            elif opc == 3:
+                print("Editar libro")
+                flg01 = False
+                flg013 = True
+            elif opc == 4:
+                flg01 = False
+                flg00 = True  # Regresar al menú principal
+
+        # Sección de buscar libro
+        while flg012:
+            print(menu012)
+            opc = input("Opción: ")
+            if not opc.isdigit():
+                print("La opción debe ser numérica.")
+            elif int(opc) < 1 or int(opc) > 4:
+                print("Opción fuera de rango.")
+            else:
+                opc = int(opc)
+                if opc == 1:
+                    print("Buscar por título")
+                elif opc == 2:
+                    print("Buscar por autor")
+                elif opc == 3:
+                    print("Buscar por género")
+                elif opc == 4:
+                    flg012 = False
+                    flg01 = True  # Regresar al menú de gestión de libros
+
+        # Sección de editar libro
+        while flg013:
+            print(menu013)
+            opc = input("Opción: ")
+            if not opc.isdigit():
+                print("La opción debe ser numérica.")
+            elif int(opc) < 1 or int(opc) > 4:
+                print("Opción fuera de rango.")
+            else:
+                opc = int(opc)
+                if opc == 1:
+                    print("Añadir nuevo libro")
+                elif opc == 2:
+                    print("Editar libro existente")
+                    flg013 = False
+                    flg0132 = True
+                elif opc == 3:
+                    print("Eliminar libro")
+                elif opc == 4:
+                    flg013 = False
+                    flg01 = True  # Regresar al menú de gestión de libros
+
+            # Sección de editar libro existente
+            while flg0132:
+                print(menu0132)
+                opc = input("Opción: ")
+                if not opc.isdigit():
+                    print("La opción debe ser numérica.")
+                elif int(opc) < 1 or int(opc) > 3:
+                    print("Opción fuera de rango.")
+                else:
+                    opc = int(opc)
+                    if opc == 1:
+                        print("Editar título")
+                    elif opc == 2:
+                        print("Editar género")
+                    elif opc == 3:
+                        flg0132 = False
+                        flg013 = True  # Regresar al menú de editar libro
+
+    # GESTIÓN USUARIOS
+    while flg02:
+        print(menu02)
+        opc = input("Opción: ")
+        if not opc.isdigit():
+            print("La opción debe ser numérica.")
+        elif int(opc) < 1 or int(opc) > 4:
+            print("Opción fuera de rango.")
+        else:
+            opc = int(opc)
+            if opc == 1:
+                print("Listar usuarios")
+                flg02 = False
+                flg021 = True
+            elif opc == 2:
+                print("Buscar usuario")
+                flg02 = False
+                flg022 = True
+            elif opc == 3:
+                print("Editar usuario")
+                flg02 = False
+                flg023 = True
+            elif opc == 4:
+                flg02 = False
+                flg00 = True  # Regresar al menú principal
+
+        # Sección de listar usuarios
+        while flg021:
+            print(menu021)
+            opc = input("Opción: ")
+            if not opc.isdigit():
+                print("La opción debe ser numérica.")
+            elif int(opc) < 1 or int(opc) > 5:
+                print("Opción fuera de rango.")
+            else:
+                opc = int(opc)
+                if opc == 1:
+                    print("Listar usuarios por DNI")
+                elif opc == 2:
+                    print("Listar usuarios por nombre")
+                elif opc == 3:
+                    print("Listar usuarios por cantidad de libros prestados")
+                elif opc == 4:
+                    print("Listar usuarios por cantidad de libros leídos")
+                elif opc == 5:
+                    flg021 = False
+                    flg02 = True  # Regresar al menú de gestión de usuarios
+
+        # Sección de buscar usuarios
+        while flg022:
+            print(menu022)
+            opc = input("Opción: ")
+            if not opc.isdigit():
+                print("La opción debe ser numérica.")
+            elif int(opc) < 1 or int(opc) > 3:
+                print("Opción fuera de rango.")
+            else:
+                opc = int(opc)
+                if opc == 1:
+                    print("Buscar por nombre")
+                elif opc == 2:
+                    print("Buscar por edad")
+                elif opc == 3:
+                    flg022 = False
+                    flg02 = True  # Regresar al menú de gestión de usuarios
+
+        # Sección de editar usuarios
+        while flg023:
+            print(menu023)
+            opc = input("Opción: ")
+            if not opc.isdigit():
+                print("La opción debe ser numérica.")
+            elif int(opc) < 1 or int(opc) > 4:
+                print("Opción fuera de rango.")
+            else:
+                opc = int(opc)
+                if opc == 1:
+                    print("Añadir usuario nuevo")
+                elif opc == 2:
+                    print("Modificar usuario existente")
+                    flg023 = False
+                    flg0232 = True
+                elif opc == 3:
+                    print("Eliminar usuario")
+                elif opc == 4:
+                    flg023 = False
+                    flg02 = True  # Regresar al menú de gestión de usuarios
+
+            # Sección de modificar usuario existente
+            while flg0232:
+                print(menu0232)
+                opc = input("Opción: ")
+                if not opc.isdigit():
+                    print("La opción debe ser numérica.")
+                elif int(opc) < 1 or int(opc) > 4:
+                    print("Opción fuera de rango.")
+                else:
+                    opc = int(opc)
+                    if opc == 1:
+                        print("Editar datos personales")
+                        flg0232 = False
+                        flg02321 = True
+                    elif opc == 2:
+                        print("Editar préstamos")
+                        flg0232 = False
+                        menu02322 = True
+                    elif opc == 3:
+                        print("Añadir libro leído")
+                    elif opc == 4:
+                        flg0232 = False
+                        flg023 = True  # Regresar al menú de editar usuarios
+                while flg02321:
+                    print(menu02321)
+                    opc = input("Opción: ")
+                    if not opc.isdigit():
+                        print("La opción debe ser numérica.")
+                    elif int(opc) < 1 or int(opc) > 3:
+                        print("Opción fuera de rango.")
+                    else:
+                        opc = int(opc)
+                        if opc == 1:
+                            print("Teléfono")
+                        elif opc == 2:
+                            print("Email")
+                        elif opc == 3:
+                            flg02321 = False
+                            flg0232 = True
+
+                while flg02322:
+                    print(menu02322)
+                    opc = input("Opción: ")
+                    if not opc.isdigit():
+                        print("La opción debe ser numérica.")
+                    elif int(opc) < 1 or int(opc) > 3:
+                        print("Opción fuera de rango.")
+                    else:
+                        opc = int(opc)
+                        if opc == 1:
+                            print("Añadir libro prestado")
+                        elif opc == 2:
+                            print("Eliminar libro prestado")
+                        elif opc == 3:
+                            flg02322 = False
+                            flg0232 = True
+
+    # GESTIÓN DE GÉNEROS
+    while flg03:
+        print(menu03)
+        opc = input("Opción: ")
+        if not opc.isdigit():
+            print("La opción debe ser numérica.")
+        elif int(opc) < 1 or int(opc) > 5:
+            print("Opción fuera de rango.")
+        else:
+            opc = int(opc)
+            if opc == 1:
+                print("Listar todos los géneros")
+            elif opc == 2:
+                print("Añadir nuevo género")
+            elif opc == 3:
+                print("Editar género")
+            elif opc == 4:
+                print("Eliminar género")
+            elif opc == 5:
+                flg03 = False
+                flg00 = True  # Regresar al menú principal
